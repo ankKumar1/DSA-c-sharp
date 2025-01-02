51598c44ff4982aa3e33a5cc44ed4ec2ab314ac1
Ankit <iamankitkumar546@gmail.com>
2025-01-02 22:42:13 +0530

    Add project files.

---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DynamicProgramming;
+
+Console.WriteLine("Hello, World!");
+var rodCutting = new RodCutting();
+rodCutting.Main();
---
+++ DynamicProgramming/RodCutting.cs
@@ -0,0 +1,48 @@
+﻿namespace DynamicProgramming
+{
+    // find maximum revenue after cutting rod of length n where cost of cutting is given at each length. 
+    public class RodCutting
+    {
+        public void Main()
+        {
+            // price of rod cutting of length i, i is index;
+            int[] costs = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24];
+            //rod length
+            int length = 5;
+            Console.WriteLine($"Maximum Profit Recursive: {MaxRevenue(costs, length)}");
+            Console.WriteLine($"Maximum Profit DP: {MaxRevenueDP(costs, length)}");
+        }
+
+        //Recursion Method
+        public static int MaxRevenue(int[] costs, int n)
+        {
+            if (n == 0) return 0;
+
+            int profit = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                profit = Math.Max(profit, costs[i] + MaxRevenue(costs, n - i));
+            }
+            return profit;
+        }
+
+        //DP method - Bottom-up
+        public static int MaxRevenueDP(int[] costs, int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = costs[0];
+
+            for (int i = 1; i <= n; i++)
+            {
+                int profit = int.MinValue;
+                for (int j = 1; j <= i; j++)
+                {
+                    profit = Math.Max(profit, costs[j] + dp[i - j]);
+                }
+                dp[i] = profit;
+            }
+            return dp[n];
+        }
+    }
+}
